e83aad578f16b3e56f8d1c70bbbbf80ae87900ec
Dominic Zappala <dominic.zappala@gmail.com>
2024-09-13 17:08:38 -0400

    fix: cmake

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/dom/projects/pace/paralell/cstar
+# Install script for directory: /home/dom/projects/pace/parallel/cstar
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/dom/projects/pace/paralell/cstar/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/dom/projects/pace/parallel/cstar/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
